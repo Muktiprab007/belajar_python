c49d27890f4a53fd14c76690d8d48dac0d19849f
muktiprb <112567095+Muktiprab007@users.noreply.github.com>
2026-02-15 13:51:00 +0700

    Add files via upload

---
+++ denah - nested loops.py
@@ -0,0 +1,25 @@
+denah = [
+    [1, 0, 1], # Baris 0
+    [0, 0, 0], # Baris 1
+    [1, 1, 0]  # Baris 2
+]
+
+baris = int(input('Masukkan Baris(0-2): '))
+kursi = int(input('Masukkan Kursi(0-2): '))
+
+if denah[baris][kursi] == 0:
+  denah[baris][kursi] = 1
+else:
+  print('Maaf Kursi Sudah Penuh')
+
+#denah kursi terbaru
+for b in denah:
+  print(b)
+
+kosong = 0
+for b in denah:
+  for k in denah:
+    if k == 0:
+      kosong += 1
+
+print(f"\nSisa kursi kosong: {kosong}")
